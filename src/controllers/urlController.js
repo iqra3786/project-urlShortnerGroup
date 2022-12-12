57d89f50e4440112ddf9f952b91cea1b91f7fc10
iqra3786 <iqra3786@gmail.com>
2022-12-12 17:11:09 +0530

    addModules

---
+++ src/controllers/urlController.js
@@ -0,0 +1,10 @@
+const urlModel=require('../models/urlModel')
+const mongoose=require("mongoose")
+
+
+
+const createUrl=async function(req,res){
+    let url=req.body
+    
+
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const route = require('./routes/route')
+app.use(express.json())
+mongoose.connect('mongodb+srv://Iqra:Iqra3786@cluster0.tsgwccn.mongodb.net/group13Database')
+    .then(() => console.log("mongodb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route)
+app.listen((3000), function () {
+    console.log("running on port" ,3000)
+})
+
