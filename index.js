1b2c83a89b1e6714b8da1f5b4fcda74f433ba977
Iqra <91978052+iqra3786@users.noreply.github.com>
2023-04-18 13:52:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const app = express()
 const mongoose = require('mongoose')
 const route = require('./src/routes/route')
 app.use(express.json())
-const cors = require('cors')
-app.use(cors())
 mongoose.connect('mongodb+srv://Iqra:Iqra3786@cluster0.tsgwccn.mongodb.net/group13Database',{
     useNewUrlParser: true
 },mongoose.set('strictQuery', true))
-
-    .then(() => console.log("mongodb is connected"))
-    .catch(err => console.log(err))
+ .then(() => console.log("mongodb is connected"))
+.catch(err => console.log(err))
 
 app.use('/', route)
 app.listen((3000), function () {
